7dde2009494d4604097983621bd5c7ddaa803c77
pnikrat <sniffoza@gmail.com>
2015-12-31 15:18:04 +0100

    Functional test of adding list element

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,17 +17,29 @@ class NewVisitorTest(unittest.TestCase):
 
 		# Zwrocila uwage, ze tytul strony i naglowek zawieraja slowo Listy.
 		self.assertIn('Listy', self.browser.title)
-		self.fail('Zakonczenie testu!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('Listy', header_text)
+
 		# Od razu zostaje zachecona, aby wpisac rzecz do zrobienia.
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'), 'Wpisz rzecz do zrobienia')
 
 		# W polu tekstowym wpisala "Kupic pawie piora" (hobby Edyty
 		# polega na tworzeniu ozdobnych przynet).
+		inputbox.send_keys('Kupić pawie pióra')
 
 		# Po nacisnieciu klawisza Enter strona zostala uaktualniona i wyswietla
 		# "1: Kupic pawie piora" jako element listy rzeczy do zrobienia.
+		inputbox.send_keys(Keys.ENTER)
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_tag_name('tr')
+		self.assertTrue(
+				any(row.text == '1: Kupić pawie pióra' for row in rows)
+			)
 
 		# Na stronie nadal znajduje sie pole tekstowe zachecajace do podania kolejnego zadania.
 		# Edyta wpisala "Uzyc pawich pior do zrobienia przynety" (Edyta jest niezwykle skrupulatna)
+		self.fail('Zakończenie testu!')
 
 		# Strona zostala ponownie uaktualniona i teraz wyswietla dwa elementy na liscie rzeczy do zrobienia.
 
