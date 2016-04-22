591ee01a7e486816705b304381943ae1fcb160dc
pnikrat <sniffoza@gmail.com>
2016-04-22 21:51:09 +0200

    added POST request to fill table in template

--- functional_tests.py
+++ functional_tests.py
@@ -11,45 +11,52 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		# Edyta dowiedziala sie o nowej, wspanialej aplikacji w postaci listy rzeczy do zrobienia.
-		# Postanowila wiec przejsc na strone glowna tej aplikacji.
+		# Edith has heard about a cool new online to-do app.
+		# She goes to check out its homepage
 		self.browser.get('http://localhost:8000')
 
-		# Zwrocila uwage, ze tytul strony i naglowek zawieraja slowo Listy.
-		self.assertIn('Listy', self.browser.title)
+		# She notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('lista', header_text)
+		self.assertIn('To-Do', header_text)
 
-		# Od razu zostaje zachecona, aby wpisac rzecz do zrobienia.
+		# She is invited to enter a to-do item straight away
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(inputbox.get_attribute('placeholder'), 'Wpisz rzecz do zrobienia')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'), 
+			'Enter a to-do item')
 
-		# W polu tekstowym wpisala "Kupic pawie piora" (hobby Edyty
-		# polega na tworzeniu ozdobnych przynet).
-		inputbox.send_keys('Kupić pawie pióra')
+		# She types "Buy peacock feathers" into a text box
+		inputbox.send_keys('Buy peacock feathers')
 
-		# Po nacisnieciu klawisza Enter strona zostala uaktualniona i wyswietla
-		# "1: Kupic pawie piora" jako element listy rzeczy do zrobienia.
+		# When she hits enter, the page updates, the page updates
+		# and now the page lists "1: Buy peacock feathers" as an item
+		# in a to-do list table
 		inputbox.send_keys(Keys.ENTER)
+
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-				any(row.text == '1: Kupić pawie pióra' for row in rows),
-				"Nowy element nie znajduje sie w tabeli."
-			)
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
-		# Na stronie nadal znajduje sie pole tekstowe zachecajace do podania kolejnego zadania.
-		# Edyta wpisala "Uzyc pawich pior do zrobienia przynety" (Edyta jest niezwykle skrupulatna)
-		self.fail('Zakończenie testu!')
+		# There is still a text box inviting her to add another item.
+		# She enters "Use peacock feathers to make a fly"
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
 
-		# Strona zostala ponownie uaktualniona i teraz wyswietla dwa elementy na liscie rzeczy do zrobienia.
+		# The page updates again, and now shows both items on her list
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
-		# Edyta byla ciekawa, czy witryna zapamieta jej liste. Zwrocila uwage na wygenerowany dla niej
-		# unikatowy adres URL, obok ktorego znajduje sie pewien tekst z wyjasnieniem
+		# Edith wonders whether the site will remember her list. Then she sees
+		# that the site has generated a unique URL for her -- there is some
+		# explanatory text to that effect
+		self.fail('Finish the test!')
 
-		# Przechodzi pod podany adres URL i widzi wyswietlona swoja liste rzeczy do zrobienia.
+		# She visits that URL - her to-do list is still there.
 
-		# Usatysfakcjonowana kladzie sie spac.
 
 if __name__ == '__main__':
 	unittest.main()
